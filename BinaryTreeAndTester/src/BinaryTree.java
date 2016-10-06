1da123fb575188ab7eb7d0cf63854fec7421e438
NoahNMorton <noahmorton98@gmail.com>
2016-10-06 14:19:03 -0500

    Initial setup of all methods for BinaryTree

--- BinaryTreeAndTester/src/BinaryTree.java
+++ BinaryTreeAndTester/src/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created on 10/5/2016, 2:29 PM
  *
@@ -6,5 +8,73 @@
  *         Part of project BinaryTreeAndTester
  */
 
-public class BinaryTree {
+public class BinaryTree<E extends Comparable> {
+
+    private TreeNode<String> root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    private String preOrder() {
+        return null;
+    }
+    private String inOrder() {
+        return null;
+    }
+    private String postOrder() {
+        return null;
+    }
+
+    public E minValue() {
+        return null;
+    }
+    public E maxValue() {
+        return null;
+    }
+    public int maxDepth() {
+        return 0;
+    }
+    public void clear() {
+        root = null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean empty() {
+        return root==null;
+    }
+    public boolean contains() {
+        return false;
+    }
+    public boolean insert(E value) {
+        return false;
+    }
+    public boolean add(E value) {
+       return insert(value); //same as insert
+    }
+
+    public boolean remove(E value) {
+        return false;
+    }
+
+    public void printSubMenu() {
+        Scanner printChoice = new Scanner(System.in);
+        System.out.println("What kind of print? 1. Preorder, 2. Inorder, 3. Postorder.");
+        switch (printChoice.nextInt()) {
+            case 1:
+                System.out.println(preOrder());
+                break;
+            case 2:
+                System.out.println(inOrder());
+                break;
+            case 3:
+                System.out.println(postOrder());
+                break;
+            default:
+                System.out.println("Invalid choice, not printing.");
+        }
+    }
 }
--- BinaryTreeAndTester/src/Mainfile.java
+++ BinaryTreeAndTester/src/Mainfile.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created on 10/5/2016, 2:29 PM
  *
@@ -8,7 +10,48 @@
 
 public class Mainfile {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
+        BinaryTree mTree = new BinaryTree();
+
+        do {
+            System.out.print("\n-Menu-\n");
+
+            System.out.println("1. Print\n" +
+                    "2. Size\n" + "3. Clear\n" + "4. Empty\n" + "5. Insert\n" + "6. Contains\n" + "7. Minimum Val\n" +
+                    "8. Max val\n" + "9. Max depth\n" + "0. Exit");
+
+            byte choice = keyboard.nextByte();
+
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1: //print
+                    mTree.printSubMenu();
+                    break;
+                case 2: //size
+                    break;
+                case 3: //clear
+                    break;
+                case 4: //empty
+                    break;
+                case 5: //insert
+                    break;
+                case 6: //contains
+                    break;
+                case 7: //minimum val
+                    break;
+                case 8:  //max val
+                    break;
+                case 9: //max depth
+                    break;
+                default:
+                    System.err.println("Invalid choice.");
+                    break;
+            }
+        }while (true);
 
     }
+
 }
